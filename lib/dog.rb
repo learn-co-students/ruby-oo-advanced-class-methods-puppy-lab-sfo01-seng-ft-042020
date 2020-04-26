8d5abd034a0080bb02d8d95fbcfb80218ddd1778
github username <you@example.com>
2020-04-26 01:19:53 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @save = save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each {|element| puts element.name}
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+    
+end
